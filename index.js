f22ed43d1a4a2a046866a5e775466456805ce6d9
gkiarie1 <gkaris24@gmail.com>
2024-01-05 08:47:24 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+
+function add(){}
+
+function subtract(){}
+
+function multiply(){}
+
+function divide(){}
+
+function add(a,b){
+    return a+b;
+}
+beforeEach()
+
+function subtract(a,b){
+    return a - b;
+}
+beforeEach()
+
+function multiply(a,b){
+    return a*b;
+}
+beforeEach()
+
+function divide(a,b){
+    return a / b;
+}
+
+function increment(n){
+return n+1;
+}
+
+
+function decrement(n){
+    return n-1;
+    }
+
+beforeEach();
+
+function makeInt(n){
+  return parseInt(n, 10);
+ }
+
+function preserveDecimal(n) {
+return parseFloat(n)
+}
